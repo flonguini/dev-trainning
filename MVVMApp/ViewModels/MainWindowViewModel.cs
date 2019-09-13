d5f342f5337bcf6e3581f4bac44c8633d9d7a0de
Fernando Longuini <fernando.silva236@fatec.sp.gov.br>
2019-09-12 21:38:38 -0300

    RelayCommands created and change the binding properties

--- MVVMApp/ViewModels/MainWindowViewModel.cs
+++ MVVMApp/ViewModels/MainWindowViewModel.cs
@@ -1,26 +1,115 @@
-﻿namespace MVVMApp.ViewModels
+﻿using MVVMApp.Commands;
+using MVVMApp.Enums;
+using MVVMApp.Models;
+using System.Windows.Input;
+
+namespace MVVMApp.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        //Dúvidas
+        //================================================================
+        //   1. O meu modelo (Person) possui 3 propriedades, aqui na VM eu posso criar uma full propertie e fazer 
+        // o binding na view como {Person.Nome}, ou eu posso criar três propriedades e fazer o binding
+        // utilizando somente essa propriedade {Binding Name}.
+        //   Para eu poder utilizar o data binding na forma {Person.Nome} seria preciso implementar INotifyPropertyChanged
+        // também no meu modelo, porém sempre entendi que a VM é responsável por disparar esse evento.
+        //   Por outro lado, se eu tenho 30 propriedades em um Model a minha VM ficaria muito poluida. Eu preciso analisar cada caso
+        // ou é melhor deixar a interface sempre na VM?
+
         #region Private Members
 
-        private string _title = "Binding Works";
+        private Person _person;
+        private string _name;
+        private Genders _gender;
+        private int? _age;
 
         #endregion
 
         #region Public Properties
 
-        public string Title
+        public Person Person
         {
-            get => _title;
+            get => _person;
             set
             {
-                if (_title == value)
+                if (_person == value)
                     return;
 
-                _title = value;
+                _person = value;
                 OnPropertyChanged();
             }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int? Age
+        {
+            get => _age;
+            set
+            {
+                if (_age == value)
+                    return;
+
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Genders Gender
+        {
+            get => _gender;
+            set
+            {
+                if (_gender == value)
+                    return;
+
+                _gender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand SaveOrUpdateCommand { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowViewModel()
+        {
+            SaveOrUpdateCommand = new RelayCommand(() => SaveOrUpdate());
+            DeleteCommand = new RelayCommand(() => Delete());
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        private void SaveOrUpdate()
+        {
+
+        }
+
+        private void Delete()
+        {
+            
         }
 
         #endregion
